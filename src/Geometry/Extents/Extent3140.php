645924a07efdad7d1c6c35175b07179295a5e599
Doug Wright <github@dougweb.org>
2021-04-14 20:21:07 +0100

    Add polygons

---
+++ src/Geometry/Extents/Extent3140.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * PHPCoord.
+ *
+ * @author Doug Wright
+ */
+declare(strict_types=1);
+
+namespace PHPCoord\Geometry\Extents;
+
+/**
+ * Asia-ExFSU/Iran - South Pars block 11.
+ * @internal
+ */
+class Extent3140
+{
+    public function __invoke(): array
+    {
+        return
+        [
+            [
+                [
+                    [52.227496, 26.589953], [52.260356, 26.562909], [52.300854, 26.548735], [52.407698, 26.464281], [52.394662, 26.537196], [52.294269, 26.617274], [52.274831, 26.632873], [52.227496, 26.589953],
+                ],
+            ],
+        ];
+    }
+}
