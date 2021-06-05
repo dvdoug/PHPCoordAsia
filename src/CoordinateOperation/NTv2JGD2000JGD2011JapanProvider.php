68e6d65b0927de41ffc32a92eb76ab46a20bbff5
Doug Wright <github@dougweb.org>
2021-06-05 18:46:59 +0100

    Add grids for Japan

---
+++ src/CoordinateOperation/NTv2JGD2000JGD2011JapanProvider.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * PHPCoord.
+ *
+ * @author Doug Wright
+ */
+declare(strict_types=1);
+
+namespace PHPCoord\CoordinateOperation;
+
+class NTv2JGD2000JGD2011JapanProvider implements GridProvider
+{
+    private NTv2Grid $cache;
+
+    public function provideGrid(): NTv2Grid
+    {
+        return $this->cache ??= new NTv2Grid(__DIR__ . '/../../resources/touhokutaiheiyouoki2011.gsb');
+    }
+}
---
+++ src/CoordinateOperation/NTv2TokyoJGD2000JapanProvider.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * PHPCoord.
+ *
+ * @author Doug Wright
+ */
+declare(strict_types=1);
+
+namespace PHPCoord\CoordinateOperation;
+
+class NTv2TokyoJGD2000JapanProvider implements GridProvider
+{
+    private NTv2Grid $cache;
+
+    public function provideGrid(): NTv2Grid
+    {
+        return $this->cache ??= new NTv2Grid(__DIR__ . '/../../resources/TKY2JGD.gsb');
+    }
+}
